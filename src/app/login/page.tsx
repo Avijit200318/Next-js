18e8d1a3ddf19a1bd0892134de0b53cfd7c04031
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2024-07-31 02:06:35 +0530

    add some basic npm pacages and create some basic file structure for frontend and backend

---
+++ src/app/login/page.tsx
@@ -0,0 +1,8 @@
+
+export default function page() {
+  return (
+    <div>
+        <h1 className="bg-blue-200">Login</h1>
+    </div>
+  )
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,7 @@
+export default function page() {
+  return (
+    <div>
+        <h1 className="">This is signup page</h1>
+    </div>
+  )
+}
