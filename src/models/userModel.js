647c51476f50f9d64195d4787c3d5212920d1928
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2024-08-03 22:13:56 +0530

    created userModel

---
+++ src/models/userModel.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        require: [true, "Please provide a username"],
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: [true, "Please provide an email"],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, "Please provide a password"],
+    },
+    isVarifided: {
+        type: Boolean,
+        default: false,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+    forgotPasswordToken: String,
+    forgotPasswordTokenExpirey: Date,
+    verifyToken: Date,
+})
+
+const User = mongoose.models.user || mongoose.model("user", userSchema);
+
+export default User;
