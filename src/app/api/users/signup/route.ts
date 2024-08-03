3853197304c9ba3f244e6cc84a673bd504229c66
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2024-08-03 22:14:23 +0530

    created singup api route

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,38 @@
+import {connect} from "../../../dbConfig/dbConfig";
+// import {connect} from "@/dbConfig/dbConfig"; if I install it during package installation
+import userModel from "../../../../models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+
+connect();
+// create the connection for mongodb
+
+export async function POST(request: NextRequest){
+    try{
+        const reqBody = await request.json();
+        const {username, email, password} = reqBody;
+        console.log(reqBody);
+
+        // check if user already exist
+        const user = await userModel.findOne({email});
+        if(user){
+            return NextResponse.json({error: "User already exists"}, {status: 400});
+        }
+
+        // hash password
+        const salt = await bcryptjs.genSalt(10);
+        const hashPassword = await bcryptjs.hash(password, salt);
+
+        const newUser = new userModel({
+            username,
+            email,
+            password: hashPassword
+        })
+
+        const savedUser = await newUser.save();
+        return NextResponse.json({message: "User created successfully", success: true, savedUser}, {status: 201});
+
+    }catch(error: any){
+        return NextResponse.json({error: error.message}, {status: 500});
+    }
+}
