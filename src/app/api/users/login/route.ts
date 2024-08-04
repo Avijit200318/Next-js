f36bf9408965ba25e0575d5c3f13238742005fe1
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2024-08-04 21:52:01 +0530

    add login api route and add it to UI

---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,41 @@
+import {connect} from "../../../dbConfig/dbConfig";
+import userModel from "../../../../models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+connect();
+// this line help us to connect to mongodb. Every time in nextjs we need to connect it
+
+export async function POST(request: NextRequest){
+    try{
+        const reqBody = await request.json();
+        const {email, password} = reqBody;
+        console.log(reqBody);
+        const user = await userModel.findOne({email});
+        if(!user){
+            return NextResponse.json({error: "User does not exist"}, {status: 400});
+        }
+
+        const validPassword = await bcryptjs.compare(password, user.password);
+        if(!validPassword){
+            return NextResponse.json({error: "Invalid password"}, {status: 400});
+        }
+
+        // create token data
+        const tokenData = {
+            id: user._id,
+            username: user.username,
+            email: user.email,
+        }
+        const token = await jwt.sign(tokenData, process.env.JWT_SECRECT!, {expiresIn: "1h"});
+
+        const res = NextResponse.json({meassage: "user login successfully", success: true}, {status: 200});
+
+        res.cookies.set("next_token", token, {httpOnly: true});
+        return res;
+
+    }catch(error: any){
+        return NextResponse.json({error: error.message}, {status: 500});
+    }
+}
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,25 +1,61 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import {axios} from "axios";
-
+import axios from "axios";
+import toast from "react-hot-toast";
 
 export default function Signin() {
-
-  
+ 
   const [user, setUser] = useState({
     email: "",
     password: "",
   });
 
-  const onLogin = async () => {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const [buttonDisabled, setButtonDisabled] = useState(true);
 
+  const onLogin = async () => {
+    try{
+      setLoading(true);
+      // const res = await axios.post("/api/users/login", user);
+
+      const res = await fetch("/api/users/login", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(user),
+      });
+
+      const data = await res.json();
+      if(data.success === false){
+        console.log("error: ", data.message);
+      }
+      console.log("login success: ", data);
+      toast.success("Login success");
+      router.push("/profile");
+    }catch(error: any){
+      console.log("login error: ", error.message);
+      toast.error(error.message);
+    }finally{
+      setLoading(false);
+    }
   }
+
+  useEffect(() => {
+    if(user.email.length > 0 && user.password.length > 0)
+    {
+      setButtonDisabled(false);
+    }else{
+      setButtonDisabled(true);
+    }
+  }, [user]);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-        <h1 className="">Login</h1>
+        <h1 className="">{loading? 'Processing...' : 'Login'}</h1>
         <hr />
         <label htmlFor="email">email:</label>
         <input id="email" type="email" onChange={(e) => setUser({...user, email: e.target.value})} value={user.email} placeholder="email" className="px-4 py-2 border border-black"/>
